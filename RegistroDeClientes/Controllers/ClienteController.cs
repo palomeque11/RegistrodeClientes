9f02434f5bb2c6cc14828087f1e1ab49f9418dea
omar palomeque <omarjacp@gmail.com>
2018-12-23 15:00:55 -0600

    aaaaa

---
+++ RegistroDeClientes/Controllers/ClienteController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using RegistroDeClientes.Models;
+
+namespace RegistroDeClientes.Controllers
+{
+    public class ClienteController : Controller
+    {
+        [HttpGet]
+        public ActionResult AgregarCliente(int id=0)
+        {
+            Cliente cliente = new Cliente();
+            return View(cliente);
+        }
+        [HttpPost]
+        public ActionResult AgregarCliente(Cliente cliente)
+        {
+            using(DbModel db = new DbModel())
+            {
+                if(db.Cliente.Any(x => x.Nombre== cliente.Nombre) && 
+                    db.Cliente.Any(x => x.Apellidos == cliente.Apellidos))
+                {
+                    ViewBag.ErrorMensage = "Cliente registrado anteriormente.";
+                    return View("AgregarCliente", cliente);
+                }
+                db.Cliente.Add(cliente);
+                db.SaveChanges();
+            }
+            ModelState.Clear();
+            ViewBag.SuccessMensage = "Registro Existoso.";
+            return View("AgregarCliente",new Cliente());
+        }
+    }
+}
